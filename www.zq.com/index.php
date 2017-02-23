3cc964b3153c53187cfa7155f52861f46f085fc3
zq.windows <zq@qq.com>
2017-02-23 10:40:09 +0800

    test more files

---
+++ www.zq.com/index.php
@@ -0,0 +1,20 @@
+
+请仔细察看一下内容.
+
+
+[0215/160345:ERROR:renderer_main.cc(200)] Running without renderer sandbox
+[0215/160345:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4194304
+[0215/160347:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+[0215/160347:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4194304
+[0215/160347:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+[0215/160347:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+[0215/160347:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4194304
+[0215/160347:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+[0215/160407:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4194304
+[0215/160613:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4194304
+[0215/160613:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+[0215/160613:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+[0215/160613:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+[0215/160613:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+[0215/160613:ERROR:child_discardable_shared_memory_manager.cc(266)] size: 4337664
+
